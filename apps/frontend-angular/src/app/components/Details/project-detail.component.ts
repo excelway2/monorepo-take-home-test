72e431e1aafc629d6b7f3cb6d3828ab9983041af
Salwa99 <s.ritage99@gmail.com>
2023-12-08 21:48:41 +0100

    Implemebt edit feature with specifying the priority of each project

--- apps/frontend-angular/src/app/components/Details/project-detail.component.ts
+++ apps/frontend-angular/src/app/components/Details/project-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; 
 import { ProjectService } from '../../project.service';
 import { Project } from '@prisma/client';
 
@@ -10,27 +11,43 @@ import { Project } from '@prisma/client';
 })
 export class ProjectDetailComponent implements OnInit {
   project: Project | undefined;
+  projectForm: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private projectService: ProjectService,
-    private router: Router
-  ) {}
+    private fb: FormBuilder
+  ) {
+    this.projectForm = this.fb.group({
+      title: ['', Validators.required],
+      description: [''],
+      priority: [''],
+    });
+  }
 
   ngOnInit(): void {
     const projectId = this.route.snapshot.paramMap.get('id');
     if (projectId) {
       this.projectService.getProjectById(projectId).subscribe((project) => {
         this.project = project;
+        this.projectForm.patchValue({
+          title: project.title,
+          description: project.description,
+          priority: project.priority,
+        });
       });
     }
   }
 
   editProject(): void {
-    if (this.project?.id) {
-      this.router.navigate(['projects', this.project.id, 'edit']);
-    }
+  if (this.project?.id) {
+    this.projectService.updateProject(this.project.id, this.projectForm.value).subscribe(() => {
+      this.router.navigate(['projects', this.project!.id]);
+    });
   }
+}
+
 
   deleteProject(): void {
     if (this.project?.id) {
