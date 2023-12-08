f5aafbf3513dd853479dff908738a845782126fe
gitnassima20 <nassimabaroudi8@gmail.com>
2023-12-08 17:57:56 +0100

    adding updated-backend

---
+++ libs/projects/src/lib/dto/project.dto.ts
@@ -0,0 +1,20 @@
+import { IsEnum, IsInt, IsNotEmpty, IsOptional, IsString } from 'node_modules/class-validator';
+import { Priority } from '@prisma/client';
+
+export class ProjectDto {
+    @IsNotEmpty()
+    @IsString()
+    title: string = '';
+  
+    @IsOptional() 
+    @IsString()
+    description?: string;
+     
+    @IsNotEmpty()
+    @IsEnum(Priority)
+    priority: Priority = Priority.LOW;
+    
+    @IsOptional()
+    @IsInt()
+    order: number = 0;
+}
--- libs/projects/src/lib/projects.controller.ts
+++ libs/projects/src/lib/projects.controller.ts
@@ -1,12 +1,59 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, UsePipes, ValidationPipe, NotFoundException, Put, Delete } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
+import { ProjectDto } from './dto/project.dto';
 
 @Controller('projects')
 export class ProjectsController {
+
   constructor(private projectsService: ProjectsService) {}
 
   @Get()
   async findAll() {
     return await this.projectsService.findAll();
   }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const project = await this.projectsService.findOne(id);
+    if (!project) {
+      throw new NotFoundException(`Project: ID ${id} not found`);
+    }
+    return project;
+  }
+
+  @Post()
+  @UsePipes(new ValidationPipe({ transform: true })) 
+  async create(@Body() createProjectDto: ProjectDto) {
+    return await this.projectsService.create(createProjectDto);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateProjectDto: ProjectDto) {
+    const updatedProject = await this.projectsService.update(id, updateProjectDto);
+    if (!updatedProject) {
+      throw new NotFoundException(`Project: ID ${id} not found`);
+    }
+    return updatedProject;
+  }
+  
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    const existingProject = await this.projectsService.findOne(id);
+
+    if (!existingProject) {
+      throw new NotFoundException(`Project: ${id} not found`);
+    }
+
+    const deletedProject = await this.projectsService.remove(id);
+    return { 
+      message: `Project: ID ${id} has been deleted successfully`,
+      deletedProject
+    };
+  }
+
+
 }
+
+
+
+
