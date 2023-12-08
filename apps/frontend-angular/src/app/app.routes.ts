5fcdfee1c04b7bdb588a615b50ffdf053fcda470
Salwa99 <s.ritage99@gmail.com>
2023-12-08 22:00:27 +0100

    Update the routes

--- apps/frontend-angular/src/app/app.routes.ts
+++ apps/frontend-angular/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Route } from '@angular/router';
 import { HomeComponent } from '../app/components/Home/home.component';
 import { ProjectDetailComponent } from '../app/components/Details/project-detail.component';
+import { AddProjectComponent } from '../app/components/AddProject/add-project.component';
 
 export const appRoutes: Route[] = [
   { path: '', component: HomeComponent },
   { path: 'projects/:id', component: ProjectDetailComponent },
+  { path: 'add-project', component: AddProjectComponent },
 ];
