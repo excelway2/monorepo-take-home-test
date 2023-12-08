ac5ae4654c2ba93f9b89c572517a3008d201851a
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-08 15:33:17 +0100

    import ProjectsModule

--- apps/backend-nest/src/app/app.module.ts
+++ apps/backend-nest/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {
   ProjectsController,
+  ProjectsModule,
   ProjectsService,
 } from '@monorepo-take-home-test/projects';
-import { PrismaModule } from '../prisma/prisma.module';
+
 
 
 @Module({
-  imports: [PrismaModule],
+  imports: [ProjectsModule],
   controllers: [AppController, ProjectsController],
   providers: [AppService, ProjectsService],
 })
