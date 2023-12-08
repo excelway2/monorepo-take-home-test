2d0a14190dec098157ab47c4c98a1b6138cbbfd2
Salwa99 <s.ritage99@gmail.com>
2023-12-08 13:16:55 +0100

    Add PrismaModule and update imports in AppModule

--- apps/backend-nest/src/app/app.module.ts
+++ apps/backend-nest/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {
   ProjectsController,
   ProjectsService,
 } from '@monorepo-take-home-test/projects';
+import { PrismaModule } from '../../../../prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController, ProjectsController],
   providers: [AppService, ProjectsService],
 })
 export class AppModule {}
+
