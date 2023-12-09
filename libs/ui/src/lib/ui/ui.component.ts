3457f587c2f3a3f04b29c818c25529b793725338
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-09 14:01:51 +0100

    add ButtonComponent to imports

--- libs/ui/src/lib/ui/ui.component.ts
+++ libs/ui/src/lib/ui/ui.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectsComponent } from '../projects/projects.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'monorepo-take-home-test-ui',
   standalone: true,
-  imports: [CommonModule, ProjectsComponent],
+  imports: [CommonModule, ProjectsComponent, ButtonComponent],
   templateUrl: './ui.component.html',
   styleUrl: './ui.component.css',
 })
