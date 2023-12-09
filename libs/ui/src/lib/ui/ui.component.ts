6203e066545ded03749374db5b51d7d7b70f7c48
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-09 16:48:50 +0100

    update paths

--- libs/ui/src/lib/ui/ui.component.ts
+++ libs/ui/src/lib/ui/ui.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProjectsComponent } from '../projects/projects.component';
-import { ButtonComponent } from '../button/button.component';
+import { ProjectsComponent } from '../lib-components/projects/projects.component';
+import { ButtonComponent } from '../lib-components/button/button.component';
+import { ProjectFormComponent } from '../lib-components/project-form/project-form.component';
 
 @Component({
   selector: 'monorepo-take-home-test-ui',
   standalone: true,
-  imports: [CommonModule, ProjectsComponent, ButtonComponent],
+  imports: [CommonModule, ProjectsComponent, ButtonComponent, ProjectFormComponent],
   templateUrl: './ui.component.html',
   styleUrl: './ui.component.css',
 })
