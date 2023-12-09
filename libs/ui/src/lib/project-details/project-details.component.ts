ba8cd946aa91375e1edbc4ce96493d4eb6eb9f61
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-09 18:01:50 +0100

    create project-details folder

---
+++ libs/ui/src/lib/project-details/project-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'monorepo-take-home-test-project-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-details.component.html',
+  styleUrl: './project-details.component.css'
+})
+export class ProjectDetailsComponent {
+
+}
