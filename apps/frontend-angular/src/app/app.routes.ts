0a00158d8d84bf9210e63f79d8b89e276b142ffe
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-09 18:00:28 +0100

    add project-form path

--- apps/frontend-angular/src/app/app.routes.ts
+++ apps/frontend-angular/src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Route } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
+import { ProjectFormComponent } from 'libs/ui/src/lib/lib-components/project-form/project-form.component';
+import { NgModule } from '@angular/core';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: 'project-form', component: ProjectFormComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })],
+})
+export class AppRoutingModule {}
